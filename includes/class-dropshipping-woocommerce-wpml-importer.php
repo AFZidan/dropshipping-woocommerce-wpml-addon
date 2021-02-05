bfff51caf0e6a586fe9826fbb3cebc3efbac3120
Suraj Rathod <36834155+suraj-rathod@users.noreply.github.com>
2021-02-05 18:01:45 +0530

    improve update file

--- includes/class-dropshipping-woocommerce-wpml-importer.php
+++ includes/class-dropshipping-woocommerce-wpml-importer.php
@@ -49,7 +49,6 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 				
 				global $sitepress,$wpdb,$woocommerce_wpml;
 
-				$single_productData = array();
 				$language_info		= icl_get_languages();
 				$language_details	= apply_filters( 'wpml_post_language_details', NULL, $product_ID ) ;
 				$active_language_code	= $sitepress->get_default_language();
@@ -63,8 +62,8 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 				if(!empty($language_info)):
 					foreach($language_info as $lang_key => $lang_info):
 						if(in_array($lang_key,$import_prd_lang)){
-
-							$post_title 								= mb_convert_encoding($single_product->name->$lang_key,"HTML-ENTITIES","UTF-8");
+							$single_productData 						= array();
+							$post_title 								= sanitize_text_field($single_product->name->$lang_key);
 							$single_productData[md5('title')]			= sanitize_text_field($post_title);
 							$single_productData[md5('post_title')]		= sanitize_text_field($post_title);
 							$single_productData[md5('slug')]			= sanitize_title($post_title);
@@ -76,6 +75,7 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 							$job_details['job_id']						= $product_ID;
 							$job_details['target']						= $lang_key;
 							$job_details['job_type']					= 'post_product';
+							$_POST['data']								= '';
 							
 							$categories_data 					= $this->product_taxonomy_data($categories,$active_language_code,$lang_key);
 							$attributes_data 					= $this->product_attributes_data($product_ID,$attributes,$active_language_code,$lang_key);
@@ -205,7 +205,7 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 		 */
 		public function post_data_string($single_product,$product_ID,$jobID,$attributes_data,$categories_data,$active_language_code,$lang_key){
 			
-			$pro_title			= $single_product->name->$lang_key;
+			$pro_title			= sanitize_text_field($single_product->name->$lang_key);
 			$pro_desc			= $single_product->description->$lang_key;
 			$pro_slug			= sanitize_title($single_product->name->$lang_key);
 			$title				= 'fields[title][data]='.$pro_title.'&fields[title][tid]=0&fields[title][format]=base64&';
@@ -243,5 +243,12 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 		}
 
 	}
+
+
+add_action( 'admin_init', 'WPML_Woocommerce_Importer' );
+function WPML_Woocommerce_Importer(){
+	return new Knawat_Dropshipping_wpml_Woocommerce_Importer();	
+}
+
 
 endif;
