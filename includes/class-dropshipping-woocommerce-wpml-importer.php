16c16ce3d295e5a262db077501bc41ede2c6f3ce
Suraj Rathod <rathodsurajsp@gmail.com>
2021-05-14 19:40:40 +0530

    change the action

--- includes/class-dropshipping-woocommerce-wpml-importer.php
+++ includes/class-dropshipping-woocommerce-wpml-importer.php
@@ -347,6 +347,7 @@ if ( class_exists( 'WCML_Editor_UI_Product_Job', false ) ) :
 
 
 	add_action( 'init', 'WPML_Woocommerce_Importer' );
+	add_action( 'admin_init', 'WPML_Woocommerce_Importer' );
 	function WPML_Woocommerce_Importer() {
 		return new Knawat_Dropshipping_wpml_Woocommerce_Importer();
 	}
